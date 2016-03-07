b6cf0a72f39987906752979edc04532e0d6df040
Robert Hajek <hajek.rob@gmail.com>
2016-03-07 12:39:11 -0700

    edited proj structure

--- app/index.js
+++ app/index.js
@@ -11,11 +11,17 @@ var GalvanizeExpreessGenerator = yeoman.generators.Base.extend({
   },
 
   createApp: function(){
-    this.copy('./src/app.jsx', './src/app.jsx');
-    this.copy('./sass/style.css', './sass/style.css');
-    this.copy('package.json');
+    this.copy('./src/app/actions/index.js', './src/app/actions/index.js');
+    this.copy('./src/components/app.js', './src/components/app.js');
+    this.copy('./style/style.css', './style/style.css')
+    this.copy('./src/reducers/index.js', './src/reducers/index.js');
+    this.copy('./test/components/app_test.js', './test/components/app_test.js');
+    this.copy('./test/test_helper.js', './test/test_helper.js');
     this.copy('.gitignore', '.gitignore');
     this.copy('gulpfile.js', 'gulpfile.js');
+    this.copy('index.html', 'index.html');
+    this.copy('package.json', 'package.json');
+    this.copy('webpack.config.js', 'webpack.config.js');
   },
 
 });
